562a7c801d3a1d8ca10a4c2309a816b3400c3da4
hitul007 <mistryhitul007@gmail.com>
2021-06-27 10:09:08 +0530

    if code not found then we will not do the token fetch API call

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -20,35 +20,38 @@ function App() {
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
-    var axios = require("axios");
-    var data = JSON.stringify({
-      grant_type: "authorization_code",
-      client_id: "DEsDngx9KAeuoJzhmR3wMuhC78NK4YUcXMOzNKt3",
-      client_secret:
-        "LVVbYw5RmKOZCDTC1CV1tQKtN4DGso0FoClAz22GRhV81Bd7egLZ2qiNoXGmqjarCDUEoXiO7F8Q27On0aK5gGosIyCG0EhnRXz3mD88SkKrcf3F3BA5Ny84NkX9MehF",
-      code: code,
-    });
 
-    var config = {
-      method: "post",
-      url: `${SSO_URL}/api/v1/user/auth/token/`,
-      headers: {
-        "Content-Type": "application/json",
-        Cookie:
-          "sessionid=g2zt7cjcpjkf2qabggfwe14yzliwjz2x; csrftoken=NcsO8L9eWK1dLfWRGQ10t2b86GzdD9vSwmzDwc77Cc4luBmQAZiYbvtfgp3X845H",
-      },
-      data: data,
-    };
-
-    axios(config)
-      .then(function(response) {
-        console.log(response.data.access_token);
-        localStorage.setItem("access_token", response.data.access_token);
-      })
-      .catch(function(error) {
-        // window.location.href = LOGIN_URL;
-        console.log(error);
+    if (code) {
+      let axios = require("axios");
+      let data = JSON.stringify({
+        grant_type: "authorization_code",
+        client_id: "DEsDngx9KAeuoJzhmR3wMuhC78NK4YUcXMOzNKt3",
+        client_secret:
+          "LVVbYw5RmKOZCDTC1CV1tQKtN4DGso0FoClAz22GRhV81Bd7egLZ2qiNoXGmqjarCDUEoXiO7F8Q27On0aK5gGosIyCG0EhnRXz3mD88SkKrcf3F3BA5Ny84NkX9MehF",
+        code: code,
       });
+
+      let config = {
+        method: "post",
+        url: `${SSO_URL}/api/v1/user/auth/token/`,
+        headers: {
+          "Content-Type": "application/json",
+          Cookie:
+            "sessionid=g2zt7cjcpjkf2qabggfwe14yzliwjz2x; csrftoken=NcsO8L9eWK1dLfWRGQ10t2b86GzdD9vSwmzDwc77Cc4luBmQAZiYbvtfgp3X845H",
+        },
+        data: data,
+      };
+
+      axios(config)
+        .then(function(response) {
+          console.log(response.data.access_token);
+          localStorage.setItem("access_token", response.data.access_token);
+        })
+        .catch(function(error) {
+          // window.location.href = LOGIN_URL;
+          console.log(error);
+        });
+    }
   });
 
   return (
