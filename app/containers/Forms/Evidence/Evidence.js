8dd75209f8a5ab111c575d097ffb342363582e1d
hitul007 <mistryhitul007@gmail.com>
2021-06-28 19:11:40 +0530

    Added update code

--- app/containers/Forms/Evidence/Evidence.js
+++ app/containers/Forms/Evidence/Evidence.js
@@ -1,126 +1,96 @@
-import React, { useState , useEffect } from 'react';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import { PapperBlock } from 'dan-components';
-import InputLabel from '@material-ui/core/InputLabel';
-import Box from '@material-ui/core/Box';
-import { spacing } from '@material-ui/system';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import { FormHelperText } from '@material-ui/core';
-import api from '../../../utils/axios';
-import FormSideBar from '../FormSideBar';
-import { EVIDENCE_FORM } from '../../../utils/constants';
-import EvidenceValidate from '../../Validator/EvidenceValidation';
-import FormHeader from '../FormHeader';
-import FormData from 'form-data';
-import fs from 'fs';
-import Upload from 'material-ui-upload/Upload';
-import {DropzoneArea} from 'material-ui-dropzone'
+import React, { useState, useEffect } from "react";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import { PapperBlock } from "dan-components";
+import InputLabel from "@material-ui/core/InputLabel";
+import Box from "@material-ui/core/Box";
+import { spacing } from "@material-ui/system";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { FormHelperText } from "@material-ui/core";
+import api from "../../../utils/axios";
+import FormSideBar from "../FormSideBar";
+import { EVIDENCE_FORM } from "../../../utils/constants";
+import EvidenceValidate from "../../Validator/EvidenceValidation";
+import FormHeader from "../FormHeader";
+// import FormData from "form-data";
+import fs from "fs";
+// import Upload from "material-ui-upload/Upload";
+import { DropzoneArea } from "material-ui-dropzone";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
-    flexDirection: 'row',
-    margin: '1rem 0',
+    flexDirection: "row",
+    margin: "1rem 0",
   },
   button: {
     margin: theme.spacing(1),
   },
   inlineRadioGroup: {
-    flexDirection: 'row',
-    gap: '1.5rem',
+    flexDirection: "row",
+    gap: "1.5rem",
   },
 }));
 const Evidence = () => {
+  // States definations.
   const [selectedDate, setSelectedDate] = React.useState(
-    new Date('2014-08-18T21:11:54')
+    new Date("2014-08-18T21:11:54")
   );
-  const [evidenceListData, setEvidenceListdata] = useState([]);
-  const [fileUploadData, setFileUploadData] = useState([]);
-  const [error, setError] = useState({});
+  let [error, setError] = useState({});
   const classes = useStyles();
-  const [detailsOfEnvAffect, setDetailsOfEnvAffect] = useState('');
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
   const [form, setForm] = React.useState({
-    available: '',
-    comment: '',
-    document: '',
+    available: "",
+    comment: "",
+    document: "",
   });
-  // console.log(evidenceListData)
-  // const fetchEvidence= async () => {
-  //   const res = await api.get(`/api/v1/incidents/91/evidences/92/`);
-  //   const result = res.data.data.results;
-  //   await setEvidenceListdata(result);
-  // };
 
-  const handleUpdateEvidence = async ()=>{
-
-    var data = new FormData();
-    data.append('evidenceCheck', form.available);
-    data.append('evidenceCategory', 'string');
-    data.append('evidenceRemark', form.comment);
-    data.append('evidenceDocument', form.document);
-    data.append('status', 'Active');
-    data.append('updatedBy', '');
-    console.log(data);
-    const res = await api.put(`/api/v1/incidents/91/evidences/92/`,data)
-    console.log(res)
-  }
-  
-  // useEffect(() => {
-  //   fetchEvidence();
-  // }, []);
-
+  // On the next button click function call.
   const handleNext = async () => {
-    
-    const { error } = EvidenceValidate(form);
+    error, isValid = EvidenceValidate(form);
     setError(error);
-    const formData = {
-      evidenceCheck: form.available,
-      evidenceNumber: 'string',
-      evidenceCategory: 'string',
-      evidenceRemark: form.comment,
-      createdBy: 0,
-      fkIncidentId: 91,
-      // evidenceDocument:  form.document,
-    };
-    // var data = new FormData();
-    // data.append('evidenceCheck', form.available);
-    // data.append('evidenceCategory', 'string');
-    // data.append('evidenceRemark', form.comment);
-    // data.append('evidenceDocument', form.document);
-    // data.append('status', 'Active');
-    // data.append('createdAt', '');
-    // data.append('createdBy', '1');
-    // data.append('updatedAt', '');
-    // data.append('updatedBy', '');
-    // data.append('fkIncidentId', '91');
-    console.log(formData);
 
-    const res = await api.post(
-      `/api/v1/incidents/91/evidences/`,
-      formData
-    );
+    if (!isValid) {
+      return;
+    }
+
+    let data = new FormData();
+    data.append("evidenceCheck", form.available);
+    data.append("evidenceCategory", "string");
+    data.append("evidenceRemark", form.comment);
+    data.append("evidenceDocument", form.document);
+    data.append("status", "Active");
+    data.append("updatedBy", "");
+
+    // If update is the case.
+    if (id) {
+      const res = await api.put(`/api/v1/incidents/91/evidences/92/`, data);
+      // If non update case is there.
+    } else {
+      data.append("createdAt", "");
+      data.append("createdBy", "1");
+      data.append("updatedAt", "");
+      data.append("updatedBy", "");
+      data.append("fkIncidentId", "91");
+      const res = await api.post(`/api/v1/incidents/91/evidences/92/`, data);
+    }
+
+    const res = await api.post(`/api/v1/incidents/91/evidences/`, formData);
     console.log(res);
     const result = res.data.data.results;
     // console.log('sagar');
     await setEvidenceListdata(result);
   };
 
-
-
   const selectValues = [1, 2, 3, 4];
-  const radioDecide = ['Yes', 'No', 'N/A'];
+  const radioDecide = ["Yes", "No", "N/A"];
   return (
     <PapperBlock title=" Evidences" icon="ion-md-list-box">
       <Grid container spacing={3}>
@@ -149,9 +119,8 @@ const Evidence = () => {
             </Box>
             <Typography variant="body2">Evidence Type 1</Typography>
           </Grid>
-          
-            
-            <Grid item md={3} justify="center">
+
+          <Grid item md={3} justify="center">
             <Box marginBottom={2}>
               <Typography variant="body">Available *</Typography>
             </Box>
@@ -170,7 +139,9 @@ const Evidence = () => {
                 />
               ))}
             </RadioGroup>
-            {error && error.available && <FormHelperText>{error.available}</FormHelperText>}
+            {error && error.available && (
+              <FormHelperText>{error.available}</FormHelperText>
+            )}
           </Grid>
           <Grid item md={4}>
             <Box marginBottom={2}>
@@ -181,7 +152,7 @@ const Evidence = () => {
               variant="outlined"
               label="Type...."
               error={error.comment}
-              helperText = {error.comment ? error.comment : ''}
+              helperText={error.comment ? error.comment : ""}
               onChange={(e) => {
                 setForm({ ...form, comment: e.target.value });
               }}
@@ -190,22 +161,23 @@ const Evidence = () => {
           <Grid item md={3}>
             <p>Attachments</p>
             <Box>
-            <input
+              <input
                 type="file"
                 name="file"
-                onChange={(e) => setForm({...form , document: e.target.files[0] })}
+                onChange={(e) =>
+                  setForm({ ...form, document: e.target.files[0] })
+                }
               />
             </Box>
             <DeleteForeverIcon />
           </Grid>
-          
-          
+
           <Grid item md={12}>
             <Button
               variant="contained"
               color="primary"
               onClick={() => handleNext()}
-              
+
               // href={
               //   Object.keys(error).length == 0
               //     ? 'http://localhost:3000/app/incident-management/registration/evidence/activity-detail/'
@@ -218,7 +190,7 @@ const Evidence = () => {
               variant="contained"
               color="primary"
               onClick={() => handleUpdateEvidence()}
-              
+
               // href={
               //   Object.keys(error).length == 0
               //     ? 'http://localhost:3000/app/incident-management/registration/evidence/activity-detail/'
