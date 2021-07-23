f1760a4d32afd9f8b6017bd9b346525922534621
vipul-digiqt <vipul@digiqt.com>
2021-07-23 17:32:41 +0530

    small fixex

--- app/containers/Forms/Evidence/Evidence.js
+++ app/containers/Forms/Evidence/Evidence.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect , useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
@@ -64,7 +64,6 @@ const Evidence = () => {
   let [error, setError] = useState({});
   const { id } = useParams();
   const history = useHistory();
-  const ref = useRef();
 
   const [evideceData, setEvideceData] = useState([]);
   const classes = useStyles();
@@ -225,6 +224,7 @@ const Evidence = () => {
 
     let tempData = [];
     if (result.length) {
+      // await setForm(result);
       for (let i = 0; i < result.length; i++) {
         tempData.push({
           evidenceCategory: result[i].evidenceCategory,
@@ -247,6 +247,45 @@ const Evidence = () => {
     await setIsLoading(true);
   };
 
+  // const fetchEvidenceData = async () => {
+  //   console.log("sagar");
+  //   const res = await api.get(
+  //     `/api/v1/incidents/${localStorage.getItem(
+  //       "fkincidentId"
+  //     )}/evidences/`
+  //   );
+  //   const result = res.data.data.results;
+
+  //   console.log(result);
+  //   let tempData = []
+  //   if(result.length){
+  //     // await setForm(result);
+  //     for(let i=0; i<result.length; i++){
+
+  //       tempData.push(
+  //         {
+  //           evidenceCategory: result[i].evidenceCategory,
+  //           evidenceCheck: result[i].evidenceCheck,
+  //           evidenceRemark: result[i].evidenceRemark,
+  //           evidenceDocument: "",
+  //           status: "Active",
+  //           createdBy: 0,
+  //           updatedBy: 0,
+  //           fkIncidentId: localStorage.getItem("fkincidentId"),
+  //           pk : result[i].id
+  //         },
+  //       )
+
+  //     }
+  //     // await setForm(tempData)
+  //     console.log(tempData)
+  //     await setEvideceData(tempData);
+
+  //   }
+
+  // await setIsLoading(true);
+  // };
+  // console.log(evideceData)
   const fetchIncidentDetails = async () => {
     const res = await api.get(
       `/api/v1/incidents/${localStorage.getItem("fkincidentId") || id}/`
@@ -332,7 +371,8 @@ const Evidence = () => {
           );
 
           if (res.status === 201) {
-            
+            // const queId = res.data.data.results.id;
+            // localStorage.setItem("id", queId);
             history.push(
               "/app/incident-management/registration/evidence/activity-detail/"
             );
@@ -344,6 +384,7 @@ const Evidence = () => {
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
+      // setOpenError(false)
       return;
     }
 
@@ -352,31 +393,21 @@ const Evidence = () => {
 
   const handleChange = async (e, index) => {
     let TempPpeData = [...form];
-    if(e.target.value === "Yes"){
-      TempPpeData[index].evidenceCheck = e.target.value;
+    TempPpeData[index].evidenceCheck = e.target.value;
+    // if (e.target.value !== "Yes"){
 
+    // }
     await setForm(TempPpeData);
-    }else {
-      TempPpeData[index].evidenceDocument = ""
-      TempPpeData[index].evidenceCheck = e.target.value;
-      ref.current.value = ""
- 
-    await setForm(TempPpeData);
-    }
-    
   };
 
   const handleFile = async (e, index) => {
     console.log(e.target.files[0]);
-    
-
     let TempPpeData = [...form];
-    
     if (
       (TempPpeData[index].evidenceDocument =
         e.target.files[0].size <= 1024 * 1024 * 25)
     ) {
-      
+
       TempPpeData[index].evidenceDocument = e.target.files[0];
       await setForm(TempPpeData);
     } else {
@@ -395,7 +426,6 @@ const Evidence = () => {
   const radioDecide = ["Yes", "No", "N/A"];
 
   useEffect(() => {
-
     fetchIncidentDetails();
     if (id) {
       fetchEvidenceList();
@@ -471,6 +501,7 @@ const Evidence = () => {
                                       defaultValue={form[index].evidenceCheck}
                                       onChange={(e) => {
                                         handleChange(e, index);
+                                        // setForm([{ ...form, evidenceCheck: e.target.value }]);
                                       }}
                                     >
                                       {radioDecide.map((value) => (
@@ -500,11 +531,12 @@ const Evidence = () => {
                                   />
                                 </TableCell>
                                 <TableCell style={{ width: "220px" }}>
+                                  {/* {form[index].evidenceDocument ?  <a target ="_blank" href={form[index].evidenceDocument}>{form[index].evidenceDocument}</a> : */}
                                   <input
                                     type="file"
-                                    ref = {ref}
                                     className={classes.fullWidth}
                                     accept=".png, .jpg , .xls , .xlsx , .ppt , .pptx, .doc, .docx, .text , .pdf ,  .mp4, .mov, .flv, .avi, .mkv"
+                                    // accept= "ppt/*  , word/*  , text , image/jpg , pdf , video/mp4,video/mov,video/flv,video/avi,video/mkv"
                                     disabled={
                                       value.evidenceCheck !== "Yes"
                                         ? true
@@ -513,8 +545,10 @@ const Evidence = () => {
                                     name="file"
                                     onChange={(e) => {
                                       handleFile(e, index);
+                                      // setForm([{ ...form, evidenceCheck: e.target.value }]);
                                     }}
                                   />
+                                  {/* } */}
                                 </TableCell>
                               </TableRow>
                             </>
@@ -546,6 +580,7 @@ const Evidence = () => {
                                     defaultValue={form[index].evidenceCheck}
                                     onChange={(e) => {
                                       handleChange(e, index);
+                                      // setForm([{ ...form, evidenceCheck: e.target.value }]);
                                     }}
                                   >
                                     {radioDecide.map((value) => (
@@ -585,6 +620,7 @@ const Evidence = () => {
                                   name="file"
                                   onChange={(e) => {
                                     handleFile(e, index);
+                                    // setForm([{ ...form, evidenceCheck: e.target.value }]);
                                   }}
                                 />
                               </TableCell>
