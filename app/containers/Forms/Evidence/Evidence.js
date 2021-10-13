b9e6c67ad898f170be21581421cfdf8617d7227d
Pramod0215 <pramodray0215@gmail.com>
2021-10-13 15:16:13 +0530

    make disable button

--- app/containers/Forms/Evidence/Evidence.js
+++ app/containers/Forms/Evidence/Evidence.js
@@ -726,6 +726,7 @@ const Evidence = () => {
                   variant="contained"
                   color="primary"
                   onClick={() => handleSubmit()}
+                  disabled={!isNext}
                 >
                   Next{isNext?null:<CircularProgress size={20}/>}
                 </Button>
