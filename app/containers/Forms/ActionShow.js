e6b70a30299aa5ef692c4e3e99dac6efeb633dee
vipu@digiqt <vipul@digiqt.com>
2021-10-14 15:55:49 +0530

    code fixes for uat

--- app/containers/Forms/ActionShow.js
+++ app/containers/Forms/ActionShow.js
@@ -1,12 +1,10 @@
-import { Grid } from "@material-ui/core";
-import Link from "@material-ui/core/Link";
+import React, { useEffect, useState, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { Button, Grid } from "@material-ui/core";
+import { defaults } from "chart.js";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 import { SSO_URL } from "../../utils/constants"
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-
 
 
 
@@ -26,22 +24,6 @@ const useStyles = makeStyles((theme) => ({
         fontSize: "14px",
         lineHeight: "1.7",
         marginTop: "10px"
-    },
-    actionLinkListSection: {
-        paddingBottom: '0px',
-        '& li': {
-            padding: '0px',
-            '& a': {
-                margin: '0px',
-            },
-        },
-    },
-    actionLinkList: {
-        marginTop: '0px',
-        '& span': {
-            fontFamily: 'Montserrat-Medium !important',
-            color: '#7692a4 !important',
-        },
     },
 }));
 
@@ -66,36 +48,26 @@ const ActionShow = (props) => {
             </Link>
         )
     }
-    console.log(props.index)
     useEffect(() => { }, [updatPage]);
 
     return (
         <Grid container spacing={3}>
-
-            <Grid item md={6} sm={6} xs={12} className={classes.actionLinkListSection} >
-                {props.title !== undefined ?
-                    <>
-                        <ListItem>
-                            <ListItemText className={classes.actionLinkList} primary={props.title} secondary={link()} />
-                        </ListItem>
-                        {/* <Grid item md={6}>
-                                    <Typography>
-                                        {props.title}
-                                    </Typography>
-                                </Grid>
-                                <Grid item md={6}>
-                                    {link()}
-                                </Grid> */}
-                    </>
-                    :
-                    <>
+            {props.title !== undefined ?
+                <>
+                    <Grid item md={6}>
+                        <Typography>
+                            {props.title}
+                        </Typography>
+                    </Grid>
+                    <Grid item md={6}>
                         {link()}
-                    </>
-                }
-            </Grid>
-
-
-
+                    </Grid>
+                </>
+                :
+                <>
+                    {link()}
+                </>
+            }
 
         </Grid>
     )
