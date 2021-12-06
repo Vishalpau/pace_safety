fc06de1221ff2e520c8795e5ea62ec0c92b0b329
vishal-teknobuilt <vishalp@teknobuilt.com>
2021-12-06 18:51:58 +0530

    working on summary page

--- app/containers/Pages/Assesments/FlhaSummary.js
+++ app/containers/Pages/Assesments/FlhaSummary.js
@@ -20,12 +20,17 @@ import HistoryIcon from '@material-ui/icons/History';
 import { PapperBlock } from 'dan-components';
 import moment from 'moment';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import CheckCircle from '@material-ui/icons/CheckCircle';
 
 import Attachment from '../../../containers/Attachment/Attachment';
 import api from '../../../utils/axios';
 import ViewHazard from './ViewHazard';
 
 import { withRouter } from 'react-router-dom';
+import flhaLogoSymbol from 'dan-images/flhaLogoSymbol.png';
+
+import CustomPapperBlock from 'dan-components/CustomPapperBlock/CustomPapperBlock';
 
 
 function TabContainer(props) {
@@ -103,6 +108,15 @@ const styles = theme => ({
     paddingRight: '20px',
     marginTop: '13px',
   },
+  statusButton: {
+    borderRadius: 4,
+    fontSize: 14,
+    width: '100%',
+    textTransform: 'none',
+    fontFamily: 'Montserrat-SemiBold !important',
+    lineHeight: '18px',
+    border: '1px solid #06425c',
+  },
   formControlTwo: {
     width: '100%',
   },
@@ -123,6 +137,13 @@ const styles = theme => ({
   table: {
     minWidth: 600,
   },
+  statusLabel: {
+    fontSize: '14px',
+    fontFamily: 'Montserrat-Regular',
+    '& svg': {
+      color: '#06425C',
+    },
+  },
 });
 
 class SimpleTabs extends React.Component {
@@ -132,9 +153,9 @@ class SimpleTabs extends React.Component {
     criticalTasks: {},
     visualConfirmations: {},
     versions: ["1.0",]
-    
+
   };
-  
+
   componentDidMount() {
     this.getFlhaDetails();
     this.getJobVisualConfirmation();
@@ -168,12 +189,12 @@ class SimpleTabs extends React.Component {
     const res = await api.get('api/v1/flhas/' + flhaId + '/visualconfirmations/');
     await this.setState({ visualConfirmations: res.data.data.results });
   }
-  redirectToHome = (Path) => { 
-    const { history } = this.props; 
-  if(history){
-   history.push(Path);
- }
-}
+  redirectToHome = (Path) => {
+    const { history } = this.props;
+    if (history) {
+      history.push(Path);
+    }
+  }
 
   render() {
     const { classes } = this.props;
@@ -182,11 +203,46 @@ class SimpleTabs extends React.Component {
     } = this.state;
 
     return (
-      <PapperBlock title={'FLHA Number:' + flha.flhaNumber} icon="ion-ios-game-controller-a-outline" desc="">
+
+      <CustomPapperBlock title={'FLHA Number:' + flha.flhaNumber} icon={flhaLogoSymbol} whiteBg>
         <Grid container spacing={1}>
           <Grid item md={9} xs={12}>
             <Paper elevation={0} className={classes.pTopandRight}>
               <Grid container spacing={3}>
+                <Grid item md={12} sm={12} xs={12} className="paddTBRemove">
+                  <Typography variant="h6" className="sectionHeading">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30.6" height="30.168" viewBox="0 0 39.6 30.168">
+                      <path id="workflow" d="M37.251,11.412l.967.967a.645.645,0,0,1,0,.925l-.78.78a5.208,5.208,0,0,1,.483,1.289H38.93a.645.645,0,0,1,.645.645V17.4a.645.645,0,0,1-.645.645h-1.1a5.176,5.176,0,0,1-.57,1.25l.715.712a.645.645,0,0,1,0,.925l-.967.967a.645.645,0,0,1-.928.013l-.78-.78a5.037,5.037,0,0,1-1.289.483v1.009a.645.645,0,0,1-.645.645H31.991a.645.645,0,0,1-.645-.645V21.512a5.3,5.3,0,0,1-1.26-.564l-.712.709a.645.645,0,0,1-.925,0l-.967-.967a.645.645,0,0,1,0-.925l.78-.78a5.082,5.082,0,0,1-.483-1.289H26.77a.645.645,0,0,1-.645-.645V15.676a.645.645,0,0,1,.645-.645h1.1a5.176,5.176,0,0,1,.57-1.25l-.712-.722a.645.645,0,0,1,0-.925l.967-.967a.645.645,0,0,1,.925,0l.78.78a5.082,5.082,0,0,1,1.289-.483V10.455a.645.645,0,0,1,.645-.645H33.7a.645.645,0,0,1,.645.645v1.1a5.176,5.176,0,0,1,1.25.57l.712-.712a.645.645,0,0,1,.922,0ZM14.2,17.081a.709.709,0,0,1-.645-.761.693.693,0,0,1,.645-.761h8.079a.712.712,0,0,1,.645.761.7.7,0,0,1-.645.761ZM8.864,14.825h2.72a.242.242,0,0,1,.255.255V17.8a.238.238,0,0,1-.255.245H8.864A.238.238,0,0,1,8.61,17.8V15.07a.242.242,0,0,1,.255-.255Zm0,6.719h2.72a.242.242,0,0,1,.255.255v2.72a.242.242,0,0,1-.255.255H8.864a.242.242,0,0,1-.255-.255v-2.73a.242.242,0,0,1,.255-.255ZM14.2,23.8a.709.709,0,0,1-.645-.757.693.693,0,0,1,.645-.761h5.511a.709.709,0,0,1,.645.761.693.693,0,0,1-.645.757ZM9.651,11.334a.435.435,0,0,1-.583-.074l-.061-.052-.812-.835a.461.461,0,0,1,.077-.645A.541.541,0,0,1,8.98,9.7l.432.458L10.995,8.7a.448.448,0,0,1,.645.151.509.509,0,0,1-.052.709L9.654,11.341Zm4.512-.645c-.355,0-.59-.355-.59-.761s.235-.761.59-.761h9.346a.712.712,0,0,1,.645.761.7.7,0,0,1-.645.761ZM1.11,22.662a3.617,3.617,0,0,1,2.6-1.35V.967C.746,1.257,1.088,4,1.107,6.549V22.662ZM4.817,3.9h27.79a.761.761,0,0,1,.548.229.773.773,0,0,1,.229.548V6.929H31.949V5.156H4.817V21.87h0a.471.471,0,0,1-.4.467c-4.228.654-4.431,5.669-.122,6.388H31.949v-2.1H33.39v2.772a.777.777,0,0,1-.229.548h0a.773.773,0,0,1-.548.229H4.253A4.953,4.953,0,0,1,.811,28.3a5.569,5.569,0,0,1-.828-3.535V6.562C-.017,3.445-.05.077,4.291,0h.058a.474.474,0,0,1,.467.477Zm28.038,9.962a2.688,2.688,0,1,1-2.688,2.688,2.688,2.688,0,0,1,2.688-2.688Z" transform="translate(0.026)" fill="#06425c" />
+                    </svg> Status & stage
+                  </Typography>
+                </Grid>
+                <Grid item md={12} sm={12} xs={12} className="paddTBRemove">
+                  <Paper elevation={1} className="paperSection">
+                    <Grid container spacing={3}>
+                      <Grid item md={12} sm={12} xs={12} className={classes.item}>
+                        <ul className="SummaryTabList">
+                          <li>
+                            <Button
+                              color="primary"
+                              variant="contained"
+                              size="small"
+                              className={classes.statusButton}
+                              onClick={(e) => {
+                                setFlhatab(true);
+                              }}
+                            >
+                              {flha.flhaStatus}
+                            </Button>
+                            <Typography className={classes.statusLabel} variant="caption" display="block" align="center">
+                              {flha.flhaStage} <CheckCircle />
+                            </Typography>
+                          </li>
+                        </ul>
+                      </Grid>
+                    </Grid>
+                  </Paper>
+                </Grid>
+
                 <Grid item xs={6}>
                   <FormLabel component="legend">Job title</FormLabel>
                   <Typography>
@@ -293,10 +349,10 @@ class SimpleTabs extends React.Component {
                       </ListItemIcon>
                       <Link
                         // onClick={()=>.push('/app/pages/assesments/flha/' + this.props.match.params.id + '/revise')}
-                        onClick={()=> this.redirectToHome('/app/pages/assesments/flha/' + this.props.match.params.id + '/revise')}
+                        onClick={() => this.redirectToHome('/app/pages/assesments/flha/' + this.props.match.params.id + '/revise')}
                         variant="subtitle"
                       >
-                        <ListItemText  primary="Revise FLHA" />
+                        <ListItemText primary="Revise FLHA" />
                       </Link>
                     </ListItem>
                     <Divider />
@@ -308,7 +364,7 @@ class SimpleTabs extends React.Component {
                         <HistoryIcon />
                       </ListItemIcon>
                       <Link
-                        onClick={()=> this.redirectToHome('/app/pages/assesments/AuditCheck/' + this.props.match.params.id + '/' + this.state.flha.fkProjectStructureIds)}
+                        onClick={() => this.redirectToHome('/app/pages/assesments/AuditCheck/' + this.props.match.params.id + '/' + this.state.flha.fkProjectStructureIds)}
                         variant="subtitle"
                       >
                         <ListItemText primary="Complete audit check" />
@@ -320,7 +376,7 @@ class SimpleTabs extends React.Component {
                         <CloseIcon />
                       </ListItemIcon>
                       <Link
-                        onClick={()=> this.redirectToHome('/app/pages/assesments/flha/' + this.props.match.params.id + '/close-out')}
+                        onClick={() => this.redirectToHome('/app/pages/assesments/flha/' + this.props.match.params.id + '/close-out')}
                         variant="subtitle"
                       >
                         <ListItemText primary="Close out" />
@@ -332,8 +388,7 @@ class SimpleTabs extends React.Component {
             </Paper>
           </Grid>
         </Grid>
-
-      </PapperBlock>
+      </CustomPapperBlock>
     );
   }
 }
