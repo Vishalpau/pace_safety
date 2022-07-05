f91249c12665cd56af0bf67fe86d54be9d079d97
Abhimanyu Soni <abhimanyushady@gmail.com>
2022-07-05 19:09:48 +0530

    Card ui changes

--- app/components/Card/Index.js
+++ app/components/Card/Index.js
@@ -4,12 +4,79 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Divider from "@material-ui/core/Divider";
 import { makeStyles } from "@material-ui/core/styles";
-import CardHeader from "./UI_2.1/CardHeader";
-import CardBody from "./UI_2.1/CardBody";
-import CardFooter from "./UI_2.1/CardFooter";
+import CardHeader from "./UI/CardHeader";
+import CardBody from "./UI/CardBody";
+import CardFooter from "./UI/CardFooter";
 import UserProfile from "../../containers/UserProfile/Index";
 import PropTypes from "prop-types";
-import Styles from "./UI_2.1/Styles";
+import Styles from "./UI/Styles";
+
+/**
+ * PaceCommonCard 
+ * Common card component for all modules
+ * @param cardTitle > string > description or title of the card
+ * @param avatar > string (link) > user image link
+ * @param username > string > user name (ex: Teknobuilt Admin)
+ * @param itemId > number > card item id
+ * @param headerFields > array of object > Card header key and value pairs
+ * @param bodyFields > array of object > Card body key and value pairs
+ * @param deleteFields > array of object > Props to pass in delete componenet
+ * @param handleVisibility > function > To show attachment box
+ * @param handleVisibilityComments > function > To show "Add comment" box
+ * @param files > number > Attachment counts
+ * @param commentsCount > number > Comments counts
+ * @param handleSummaryPush > function > Function to navigate to detail page
+ * @param checkDeletePermission > boolean > check the permission to delete
+ * @since v1.0.0
+ * @Abhimanyu <abhimanyus@teknobuilt.com>
+ * @example
+      <CardView
+        cardTitle={item.description} // Card title
+        avatar={item.avatar} // Card avatar
+        username={item.username} // Profile username
+        itemId={item.id} // Item ID
+        headerFields={[
+          // Card header labels and values for each item
+          { label: ahaLabels.header[0], value: item.ahaNumber },
+          { label: ahaLabels.header[1], value: "AHA" },
+          { label: ahaLabels.header[2], value: item.ahaStage },
+          {
+            label: ahaLabels.header[3],
+            value: item.ahaStatus === "Assessment" ? "Open" : item.ahaStatus,
+          },
+        ]}
+        bodyFields={[
+          // Card body labels and values for each item
+          { label: ahaLabels.body[0], value: item.workArea },
+          { label: ahaLabels.body[1], value: item.location },
+          {
+            label: ahaLabels.body[2],
+            value: moment(item.createdAt).format("Do MMMM YYYY, h:mm:ss a"),
+          },
+          { label: ahaLabels.body[3], value: item.createdByName },
+        ]}
+        deleteFields={{
+          // Delete component props
+          deleteUrl: `/api/v1/ahas/${item.id}/`,
+          afterDelete: () => {
+            fetchAllAHAData();
+          },
+          axiosObj: api,
+          item: deleteItem,
+          loader: setIsLoading,
+          loadingFlag: false,
+          deleteMsg: "Are you sure you want to delete this AHA?",
+          yesBtn: "Yes",
+          noBtn: "No",
+        }}
+        handleVisibility={() => handleVisibility()} // Show attachment box
+        handleVisibilityComments={() => handleVisibilityComments()} // Show "add comment" box
+        files={item.files !== null ? item.files.length : 0} // Attachment counts
+        commentsCount={item.commentsCount} // Comments count
+        handleSummaryPush={() => handleSummaryPush(item, commentPayload)} // Go to detail page function
+        checkDeletePermission={checkDeletePermission} // Check delete permission
+      />
+ **/
 
 const useStyles = makeStyles((theme) => Styles());
 
