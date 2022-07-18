896a7a54f9a2894e6278537887be7379b6f4b4a2
abhimanyusevensquare <abhi@sevensquaretech.com>
2022-07-18 15:23:21 +0530

    Commenting

--- app/components/Date/DateFormat.js
+++ app/components/Date/DateFormat.js
@@ -1,5 +1,23 @@
 import moment from "moment";
 
+/**
+ * @file - DateFormat.js
+ * @location /app/components/Date/DateFormat.js
+ * @description Serves the required date format as dd-mmm-yyyy + time
+ * @author Abhimanyu Soni <abhimanyus@teknobuilt.com>
+ * @since v1.1.0
+ **/
+
+/**
+ * function name: DateFormat
+ * function description: Serves the required date format as dd-mmm-yyyy + time
+ * @param string value - original date value with time
+ * @param boolean time - pass as true if want to show time as well (default false)
+ * @since v1.0.0
+ * @author Abhimanyu Soni <abhimanyus@teknobuilt.com>
+ * @example DateFormat(value, true/false/null);
+ **/
+
 const DateFormat = (value, time) => {
   if (time) {
     return moment(value).format("DD-MMM-YYYY, h:mm:ss a");
