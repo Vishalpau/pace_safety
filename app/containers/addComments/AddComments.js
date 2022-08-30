2796799cea6e9fb5dd4c05460d5f4da3714adcd2
mohit dubey <mohitdubey037@gmail.com>
2022-08-30 12:52:50 +0530

    added description on comments component

--- app/containers/addComments/AddComments.js
+++ app/containers/addComments/AddComments.js
@@ -8,6 +8,47 @@ import api from '../../utils/axios';
 import { CircularProgress } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
+/** COMPONENT INFORMATION
+ * CommentCard Component
+ * Comment component to add comment for all modules
+ * @since v1.0.0
+ * @mohit <mohitdubey037@gmail.com>
+ * @example
+        <AddComments
+          commentPayload={commentPayload} // payload to send for example:
+            const commentPayload = {
+                fkCompanyId: value.fkCompanyId,
+                fkProjectId: value.fkProjectId,
+                commentContext: "compliance",
+                contextReferenceIds: value.id,
+                commentTags: "",
+                comment: commentData,
+                parent: 0,
+                thanksFlag: 0,
+                status: "Active",
+                createdBy: value.createdBy,
+            };
+          commentOpen={commentsOpen} // state to open the component
+          commentData={commentData} // initial comments to add on comment payload 
+          hiddenn={hiddenn} // initial state to hide or show the component
+          isLoading={isLoading} // state to show the loader while adding the comment
+          setIsLoading={(val) => setIsLoading(val)} // function to change the state of loader i.e to make it
+            false after sending the comments
+          fetchAllData={fetchAllComplianceData} function to call after posting the comment i.e to fetchInitialData
+          handleComments={(type) => handleComments(type)} function to add the comments into comment data
+          handleVisibilityComments={handleVisibilityComments} function to make the comment visible or hide the component
+          addComments={(value) => setCommentData(value)} function to add data into the comment data
+        />
+**/
+
+/**
+ * @file - AddComments.js
+ * @location /app/containers/addComments
+ * @description FIle to post a comment
+ * @author Abhimanyu<abhimanyus@teknobuilt.com>
+ * @since v1.1.0
+ **/
+
 const useStyles = makeStyles((theme) => ({
     loadingWrapper: {
         margin: theme.spacing(1),
@@ -31,7 +72,7 @@ function AddComments(props) {
         if (props.commentData) {
             props.setIsLoading(true);
             await api.post("/api/v1/comments/", props.commentPayload)
-                .then(async(res) => {
+                .then(async (res) => {
                     await props.fetchAllData();
                     props.setIsLoading(false);
                 })
